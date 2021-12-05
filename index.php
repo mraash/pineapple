cb511b659cc482ffbc6ea97bcacbb34d9f934ec9
Marshans <marshansr@gmail.com>
2021-12-06 01:14:07 +0200

    hide errors

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+ini_set('display_errors', '0');
 
 
 require_once __DIR__ . '/app/general/libraries/Debug/write.php';
